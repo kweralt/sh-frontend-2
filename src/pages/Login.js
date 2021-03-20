3f1185e679d1fed0d2f0e0bb034ca4d08cebb8a6
kiawee <kiaweee@gmail.com>
2021-03-20 15:50:44 +0800

    Add new login template

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,15 @@
-import { makeStyles, Paper } from "@material-ui/core";
-import { PeopleOutlineTwoTone } from "@material-ui/icons";
-import PageHeader from "../components/PageHeader";
-import LoginForm from "../components/LoginForm";
+import {
+  makeStyles,
+  Grid,
+  Avatar,
+  Typography,
+  Button,
+  FormControlLabel,
+  Checkbox,
+  TextField,
+  Link,
+} from "@material-ui/core";
+import { LockOutlined } from "@material-ui/icons";
 import ContentWrapper from "../components/ContentWrapper";
 
 const useStyles = makeStyles((theme) => ({
@@ -14,26 +22,73 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: "column",
     alignItems: "center",
   },
+  avatar: {
+    margin: theme.spacing(1),
+  },
   form: {
     width: "100%", // IE 11
     marginTop: theme.spacing(1),
   },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
 }));
 
-export default function Login() {
+export default function Login(props) {
   const classes = useStyles();
+  const { loggedIn, login, logout } = props;
 
   return (
     <ContentWrapper>
       <div className={classes.root}>
-        <Paper className={classes.paper}>
-          <PageHeader
-            title="Login"
-            subTitle="Welcome to SingHealth Retail Audit App"
-            icon={<PeopleOutlineTwoTone fontSize="large" />}
+        <Grid className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlined />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign In
+          </Typography>
+        </Grid>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
           />
-          <LoginForm className={classes.form} />
-        </Paper>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="password"
+            label="Password"
+            name="password"
+            autoComplete="current-password"
+            autoFocus
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick={login}
+          >
+            Sign In
+          </Button>
+          <Link href="#" variant="body2">
+            Forgot password?
+          </Link>
+        </form>
       </div>
     </ContentWrapper>
   );
