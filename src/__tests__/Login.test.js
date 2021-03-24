236c189894168bce7d90376ea6bb59d93178e6e3
kiawee <kiaweee@gmail.com>
2021-03-24 22:00:50 +0800

    adadd sample tests

---
+++ src/__tests__/Login.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Login from "../pages/Login";
+
+describe("Login input validation", () => {
+  describe("with valid inputs", () => {
+    it.todo("calls the onSubmit function");
+  });
+
+  describe("with invalid email", () => {
+    it.todo("renders the email validation error");
+  });
+
+  describe("with invalid password", () => {
+    it.todo("renders the password validation error");
+  });
+});
