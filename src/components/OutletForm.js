041c0d764c3b269bde05bd5cd22c4252f12bf8d3
Caryl <caryl830@gmail.com>
2021-03-31 23:50:11 +0800

    Replace Institution text field with dropdown

--- src/components/OutletForm.js
+++ src/components/OutletForm.js
@@ -1,30 +1,65 @@
 import { useForm, Form } from "../components/useForm";
 import { Grid } from "@material-ui/core";
 import Controls from "./controls/Controls";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import * as directoryServices from "../services/directoryServices";
+import dateRangeValidation from "../utils/dateRangeValidation";
+import _, {keys} from "underscore";
 
 const initialFormValues = {
-    institutionname: "",
+    institution: "",
     outletname: "",
+    outlettype: "",
     email: "",
     unitnumber: "",
     tenancystart: new Date(),
     tenancyend: new Date()
 };
 
+const changeJSONKeys = (vals) => {
+    const data = [];
+    const prevKeys = _.keys(vals[0]);
+    for (let index = 0; index < vals.length; index++) {
+        var item = vals[index];
+        data[index] = {
+            id: item[prevKeys[0]],
+            title: item[prevKeys[1]]
+        };
+        
+    }
+    return data;
+};
+
 export default function OutletForm(props) {
     const { addOrEdit, recordForEdit } = props;
+    const [institutions, setInstitutions] = useState([]);
+    const [outletTypes, setOutletTypes] = useState([]);
 
     const validInputs = (fieldValues = values) => {
         let temp = {...errors};
 
+        console.log(fieldValues);
+
         // TODO: Finish input validation
-        if ("institutionname" in fieldValues) {
-            temp.institutionname = fieldValues.institutionname ? "" : "This field is required";
+        if ("institution" in fieldValues) {
+            temp.institution = fieldValues.institution ? initialFormValues.institution : "This field is required";
         }
         if ("outletname" in fieldValues) {
-            temp.outletname = fieldValues.outletname ? "" : "This field is required";
+            temp.outletname = fieldValues.outletname ? initialFormValues.outletname : "This field is required";
+        }
+        if ("unitnumber" in fieldValues) {
+            temp.unitnumber = fieldValues.unitnumber ? initialFormValues.unitnumber : "This field is required";
+        }
+        if ("email" in fieldValues) {
+            temp.email = /$^|.+@.+..+/.test(fieldValues.email) ? initialFormValues.email : "Invalid email";
+        }
+        if ("outlettype" in fieldValues) {
+            temp.outlettype = fieldValues.outlettype ? initialFormValues.outlettype : "This field is required";
+        }
+        if (("tenancystart" in fieldValues) && ("tenancyend" in fieldValues)) {
+            let validDateRange = dateRangeValidation(fieldValues.tenancystart, fieldValues.tenancyend);
+            temp.tenancystart = validDateRange ? "" : "Invalid date range";
+            temp.tenancyend = validDateRange ? "" : "Invalid date range";
         }
 
         setErrors({
@@ -41,7 +76,7 @@ export default function OutletForm(props) {
         setErrors,
         handleInputChange,
         resetForm
-    } = useForm(initialFormValues, true, validInputs);
+    } = useForm(initialFormValues, false, validInputs);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -59,7 +94,21 @@ export default function OutletForm(props) {
         }
     };
 
-    useEffect(() => {
+    const getInstitutions = async () => {
+        const data = await directoryServices.getInstitutions();
+        const institutionData = changeJSONKeys(data.institutions);
+        setInstitutions(institutionData);
+    };
+
+    const getOutletTypes = async () => {
+        const data = await directoryServices.getOutletTypes();
+        const typesData = changeJSONKeys(data.data);
+        setOutletTypes(typesData);
+    };
+
+    useEffect(async () => {
+        await getInstitutions();
+        await getOutletTypes();
         if (recordForEdit != null) {
             setValues({
                 ...recordForEdit,
@@ -70,12 +119,14 @@ export default function OutletForm(props) {
     return (
         <Form onSubmit={handleSubmit}>
             <Grid container>
-                <Controls.Input
-                        name="institutionname"
+                <Grid item sm={6}>
+                    <Controls.Select
+                        name="institution"
                         label="Institution"
-                        value={values.institutionname}
+                        value={values.institution}
+                        error={errors.institution}
                         onChange={handleInputChange}
-                        error={errors.institutionname}
+                        options={institutions}
                     />
                     <Controls.Input
                         name="outletname"
@@ -98,6 +149,16 @@ export default function OutletForm(props) {
                         onChange={handleInputChange}
                         error={errors.email}
                     />
+                </Grid>
+                <Grid item sm={6}>
+                    <Controls.RadioGroup
+                        name="outlettype"
+                        label="Outlet Type"
+                        value={values.outlettype}
+                        error={errors.outlettype}
+                        onChange={handleInputChange}
+                        items={outletTypes}
+                    />
                     <Controls.DatePicker
                         name="tenancystart"
                         label="Tenancy Start"
@@ -109,12 +170,8 @@ export default function OutletForm(props) {
                         label="Tenancy End"
                         value={values.tenancyend}
                         onChange={handleInputChange}
-                    />
-                {/* <Grid item xs={6}>
+                    />                 
                 </Grid>
-                <Grid item xs={6}>
-
-                </Grid> */}
                 <Grid item lg={12}>
                     <div>
                         <Controls.Button type="submit" text="Submit" />
