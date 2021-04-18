a444b2ae13b1582058b713bb84bdd534c1a7a26b
kiawee <kiaweee@gmail.com>
2021-04-18 23:14:03 +0800

    login test

--- src/__tests__/TenantAccountTest.js
+++ src/__tests__/TenantAccountTest.js
@@ -1,24 +1,18 @@
 const assert = require("assert");
-const { Builder, Key, By, until } = require("selenium-webdriver");
-const chrome = require("selenium-webdriver/chrome");
+const webdriver = require("selenium-webdriver"),
+  By = webdriver.By,
+  until = webdriver.until;
 
 (async function TenantAccountTest() {
-  chrome.setDefaultService(
-    new chrome.ServiceBuilder("../chromedriver.exe").build()
-  );
-  let driver = await new Builder().forBrowser("chrome").build();
+  const driver = await new webdriver.Builder().forBrowser("firefox").build();
 
-  driver.manage().window().maximize();
-
-  // test 1: assert if(!token){return <Login />}
-  console.log("TESTING: Enter homepage without login, redirected to login");
+  // test 1: login test
   await driver.get("http://localhost:3000");
-  // try to go to tenants page without login token
-  await driver.get("http://localhost:3000/tenants");
-  driver.sleep(9000);
-  let currentURL = await driver.getCurrentUrl();
-  assert.equal(currentURL, "http://localhost:3000");
-  console.log("great success");
+  await driver
+    .findElement(By.id("email"))
+    .sendKeys("patrick@squidwardcommunitycollege.edu");
+  await driver.findElement(By.id("password")).sendKeys("patrick");
+  await driver.findElement(By.name("btnK")).click();
 
   await driver.sleep(3000);
   driver.quit();
